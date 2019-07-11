68a9ecbb05e17728b8d8118a0fc12c4453056013
mariamabd <52761105+mariamabd@users.noreply.github.com>
2019-07-11 15:47:54 -0500

    Add files via upload

--- blog.js
+++ blog.js
@@ -1,5 +1,8 @@
-body {
-
-onload.window.open('Hello');
-
-}
+var quotes = [
+  'Keep your face always toward the sunshine—and shadows will fall behind you. —Walt Whitman',
+  'It is always the simple that produces the marvelous. —Amelia Barr'.
+  'Let us make our future now, and let us make our dreams tomorrow’s reality. —Malala Yousafzai',
+  'All you need is the plan, the road map, and the courage to press on to your destination. —Earl Nightingale',
+  'Once we believe in ourselves, we can risk curiosity, wonder, spontaneous delight, or any experience that reveals the human spirit. —E. E. Cummings',
+  'Happiness is not something you postpone for the future; it is something you design for the present. —Jim Rohn'
+]
